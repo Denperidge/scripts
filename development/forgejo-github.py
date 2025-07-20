bf183ece3f46bc63b91774656f342dce8456badb
Denperidge <cat@denperidge.com>
2025-07-20 21:55:29 +0200

    forgejo-github: pipx & configparser
    Additionally, added .gitgignore

--- development/forgejo-github.py
+++ development/forgejo-github.py
@@ -1,29 +1,63 @@
-#!/usr/bin/env python -B
+#!/usr/bin/env -S pipx run
 
-from github import Github Auth		# https://github.com/PyGithub/PyGithub
+"""
+14-04-2019: Jan-Piet Mens - Original script
+https://jpmens.net/2019/04/15/i-mirror-my-github-repositories-to-gitea/
+
+20-07-2025: Denperidge - forgejo rename, pipx & ConfigParser adaptation
+
+Usage:
+--------
+  1. Save forgejo.github.py locally
+  2. Create `forgejo-github.conf` in the same directory.
+     See below for default contents
+  3. Run `chmod +x forgejo-github.py`
+  4. Run `./forgejo-github.py`
+
+
+forgejo-github.conf template:
+-----------------------------
+FORGEJO_URL=https://git.example.com
+FORGEJO_TOKEN=your_token_here
+GITHUB_USERNAME=your_username_here
+GITHUB_TOKEN=another_token_here
+"""
+
+# pipx metadata
+# /// script
+# dependencies = ["requests", "PyGithub"]
+# ///
+
+from github import Auth		# https://github.com/PyGithub/PyGithub
 import requests
+from configparser import ConfigParser, UNNAMED_SECTION
 import json
 import sys
 import os
 
-gitea_url = "http://127.0.0.1:3000/api/v1"
-gitea_token = open(os.path.expanduser("~/.gitea-api")).read().strip()
+config = ConfigParser(allow_unnamed_section=True)
+config.read("forgejo-github.conf")
+config = config[UNNAMED_SECTION]
+
+forgejo_url = f"${config["FORGEJO_URL"]}/api/v1"
+forgejo_token = config["FORGEJO_TOKEN"]
+
+github_username = config["GITHUB_USERNAME"]
+github_token = config["GITHUB_TOKEN"]
+
 
 session = requests.Session()        # Gitea
 session.headers.update({
     "Content-type"  : "application/json",
-    "Authorization" : "token {0}".format(gitea_token),
+    "Authorization" : "token {0}".format(forgejo_token),
 })
 
-r = session.get("{0}/user".format(gitea_url))
+r = session.get("{0}/user".format(forgejo_url))
 if r.status_code != 200:
     print("Cannot get user details", file=sys.stderr)
     exit(1)
 
-gitea_uid = json.loads(r.text)["id"]
-
-github_username = "jpmens"
-github_token = open(os.path.expanduser("~/.github-token")).read().rstrip()
+forgejo_uid = json.loads(r.text)["id"]
 
 auth = Auth.Token(token)
 gh = Github(auth=auth)
@@ -37,7 +71,7 @@ for repo in gh.get_user().get_repos():
             "clone_addr"        : repo.clone_url,
             "mirror"            : True,
             "private"           : repo.private,
-            "uid"               : gitea_uid,
+            "uid"               : forgejo_uid,
         }
 
         if repo.private:
@@ -46,7 +80,7 @@ for repo in gh.get_user().get_repos():
 
         jsonstring = json.dumps(m)
 
-        r = session.post("{0}/repos/migrate".format(gitea_url), data=jsonstring)
+        r = session.post("{0}/repos/migrate".format(forgejo_url), data=jsonstring)
         if r.status_code != 201:            # if not CREATED
             if r.status_code == 409:        # repository exists
                 continue
