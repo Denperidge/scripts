a2d3b74111e9b7b5556f3649c6435266a09e5de2
Denperidge <cat@denperidge.com>
2025-08-06 20:07:01 +0200

    steam-ids: req_url based instead of app_id
    Also fixed the title regex trailing space

--- other/steam-ids.py
+++ other/steam-ids.py
@@ -18,7 +18,7 @@ SCRIPT_DIR = dirname(realpath(__file__))
 CACHE_FILE = join(SCRIPT_DIR, ".steam-id-cache.json")
 
 REQUEST_URL = "https://store.steampowered.com/app/"
-TITLE_REGEX = r"<title>(?P<title>.*?)(on Steam|)</title>"
+TITLE_REGEX = r"<title>(?P<title>.*?)( on Steam|)</title>"
 
 
 
@@ -29,17 +29,17 @@ if isfile(CACHE_FILE):
 else:
     cache = dict()
 
-def cache_and_print(app_id, title, setCache=True):
+def cache_and_print(req_url, title, setCache=True):
     if setCache:
-        cache[app_id] = title
+        cache[req_url] = title
     if title is not False:
-        print("[INFO]: {0} == {1}".format(app_id, title))
+        print("[INFO]: {0} == {1}".format(req_url, title))
     else:
-        print("[INFO]: No game found for appId {}".format(app_id))
+        print("[INFO]: No game found for {}".format(req_url))
 
-def app_info_from_steam(app_id):
+def app_info_from_steam(app_id, req_url):
     try:
-        req = get(REQUEST_URL + str(app_id))
+        req = get(req_url)
         history_count = len(req.history)
 
         if history_count < 1:
@@ -47,14 +47,14 @@ def app_info_from_steam(app_id):
             if search_for_title is not None:
                 title = search_for_title.group("title")
                 if title is not None:
-                    cache_and_print(app_id, title)
+                    cache_and_print(req_url, title)
                 else:
                     raise ValueError("[WARN]: Could not find title group for appId {}".format(app_id))
             else:
                 raise ValueError("[WARN]: Could not find title element for appId {}".format(app_id))
 
         elif history_count == 1 and req.history[0].status_code == 302 and req.url == "https://store.steampowered.com/":
-            cache_and_print(app_id, False)
+            cache_and_print(req_url, False)
         else:
             print(req.history)
             raise NotImplementedError("[ERR]: Didn't expect more than 2 redirects")
@@ -69,13 +69,14 @@ def app_info_from_steam(app_id):
     
 if __name__ == "__main__":
     for app_id in APP_ID_RANGE:
-        if str(app_id) in cache:
-            print("[INFO]: appId {} found in cache".format(app_id))
-            cache_and_print(app_id, cache[str(app_id)], False)
+        req_url = join(REQUEST_URL, str(app_id))
+        if req_url in cache:
+            print("[INFO]: {} found in cache".format(req_url))
+            cache_and_print(req_url, cache[req_url], False)
             continue
         else:
-            print("[INFO]: appId {} not found in cache".format(app_id))
-            app_info_from_steam(app_id)
+            print("[INFO]: {} not found in cache".format(req_url))
+            app_info_from_steam(app_id, req_url)
             sleep(1.2)
     
     with open(CACHE_FILE, "w", encoding="utf-8") as file:
